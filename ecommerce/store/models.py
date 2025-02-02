e44a117d1ba262dfd5ac8276d0c1a95a2e8fb89d
Dermot Kelly <47356683+DermotKelly@users.noreply.github.com>
2025-02-02 20:28:35 +0000

    First Commit

---
+++ ecommerce/store/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+
+# Create your models here.
+
+class Category(models.Model):
+    name= models.CharField(max_length=250, db_index=True) # query speed up and will not read entire database
+
+    slug = models.SlugField(max_length=250, unique=True) # Unique Category
+
+    class Meta:
+
+        verbose_name_plural = 'categories' # Change default Class name (Categorys) that apperars in admin
+
+
+    def __str__(self):
+        return self.name # Returns record by its name
+
+
+class Product(models.Model):
+
+    title = models.CharField(max_length=250)
+
+    brand = models.CharField(max_length=250, default='un-branded')
+
+    description = models.TextField(blank=True)
+
+    slug = models.SlugField(max_length=250)
+
+    price = models.DecimalField(max_digits=4, decimal_places=2)
+
+    image = models.ImageField(upload_to='images/')
+
+    class Meta:
+
+        verbose_name_plural = 'products'
+
+
+    def __str__(self):
+        return self.title
